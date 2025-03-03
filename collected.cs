fcc04b624f1048a98791ef3dc8c837b8bc3cf47a
Kim Terrence Quines <140288546+KimTerrence@users.noreply.github.com>
2025-03-03 11:41:59 +0800

    Add files via upload

---
+++ collected.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KimTerrence_Activity2.resource
+{
+    internal class collected
+    {
+        
+          public static string name { get; set; }
+          public static string col { get; set; }
+
+    }
+}
